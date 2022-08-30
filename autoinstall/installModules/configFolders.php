6802444b8a72e98ad5f72512d4fa8e3132ca4c7d
Alexis <52383750+Alexis-Elaxis@users.noreply.github.com>
2022-08-30 22:34:35 +0200

    🖊️ Adding system version section

--- autoinstall/installModules/configFolders.php
+++ autoinstall/installModules/configFolders.php
@@ -6,7 +6,7 @@ function configFolders() {
 
     if (!file_exists($path)){
         if (mkdir($path, 0777, true)) {
-            if(file_put_contents($path. 'config.json', '{"place_name":"","place_location":"","place_description":"","place_large_icon":"","place_small_icon":""}')) $c = $c + 1;
+            if(file_put_contents($path. 'config.json', '{"sys_version":"", "place_name":"","place_location":"","place_description":"","place_large_icon":"","place_small_icon":""}')) $c = $c + 1;
             if(file_put_contents($path. 'database.json', '{"serveradress":"","username":"","password":"","databse":""}')) $c = $c + 1;
 
             if(chmod($path . 'config.json', 0777)) $c = $c + 1;
